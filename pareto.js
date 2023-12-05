adb13311edf4f19db4ab57cd63b18414f2d52e08
Pradyoth S P <pradyothsp@Pradyoths-MacBook-Air.local>
2023-12-04 23:28:07 -0500

    WIP: pareto chart

--- pareto.js
+++ pareto.js
@@ -1,57 +1,115 @@
-anychart.onDocumentReady(function () {
+// Load the data from the JSON file
+d3.json("data/world_clean_dataset.json").then(function (data) {
+    // Convert string values to numbers
+    data.forEach(function (d) {
+        d.year = +d.year;
+        d.gdp = +d.gdp;
+        d.primary_energy_consumption = +d.primary_energy_consumption;
+    });
 
-    // Load data from CSV file
-    anychart.data.loadCsvFile('data/world_clean_dataset.csv', function (data) {
+    // Get unique country names
+    const countries = Array.from(new Set(data.map(d => d.country)));
 
-        // create a pareto chart
-        var chart = anychart.pareto();
-        // feed the data into the chart
-        chart.data(data);
+    // Populate the select element with country options
+    const select = d3.select("#countrySelect")
+        .selectAll("option")
+        .data(countries)
+        .enter()
+        .append("option")
+        .text(d => d);
 
-        // set the chart title
-        chart.title('Number of Deaths for 10 Leading Causes of Death in U.S. in 2019');
-        // set the measure y axis title
-        chart.yAxis(0).title('Year');
-        // set the cumulative percentage y axis title
-        chart.yAxis(1).title('Cumulative percentage');
+    // Set up the chart dimensions
+    const margin = { top: 30, right: 50, bottom: 70, left: 70 };
+    const width = 1200 - margin.left - margin.right;
+    const height = 600 - margin.top - margin.bottom;
 
-        // use one of the pre-built palettes for coloring
-        chart.palette(anychart.palettes.earth);
+    // Create color scale for bars and line
+    const colorScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.primary_energy_consumption)])
+        .range(['lightblue', 'steelblue']); // Adjust colors as needed
 
-        // configure the visual settings of the first series
-        chart.getSeries(0).normal().fill("#c98411", 0.3);
-        chart.getSeries(0).hovered().fill("#c98411", 0.1);
-        chart.getSeries(0).selected().fill("#c98411", 0.5);
-        chart.getSeries(0).normal().hatchFill("forward-diagonal", "#c98411", 1, 15);
-        chart.getSeries(0).hovered().hatchFill("forward-diagonal", "#c98411", 1, 15);
-        chart.getSeries(0).selected().hatchFill("forward-diagonal", "#c98411", 1, 15);
-        chart.getSeries(0).normal().stroke("#c98411");
-        chart.getSeries(0).hovered().stroke("#c98411", 2);
-        chart.getSeries(0).selected().stroke("#c98411", 4);
+    // Create SVG element
+    const svg = d3.select("body")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        // configure the visual settings of the second series
-        chart.getSeries(1).normal().stroke("#991e00", 4, "4 4", "round");
+    // Function to update the chart based on the selected country
+    function updateChart(selectedCountry) {
+        const filteredData = data.filter(d => d.country === selectedCountry);
 
-        // configure the pareto column series tooltip format
-        var column = chart.getSeriesAt(0);
-        column.tooltip().format('Value: {%Value}');
+        // Create x and y scales
+        const xScale = d3.scaleBand()
+            .domain(filteredData.map(d => d.year))
+            .range([0, width])
+            .padding(0.1)
+            .paddingOuter(0.2);  // Add padding to the outer edges
 
-        // configure the pareto column series tooltip format
-        var line = chart.getSeriesAt(1);
-        line
-            .tooltip()
-            .format('Cumulative Frequency: {%CF}% \n Relative Frequency: {%RF}%');
+        const yScaleGDP = d3.scaleLinear()
+            .domain([0, d3.max(filteredData, d => d.gdp)])
+            .range([height, 0]);
 
-        // set the pareto line series labels
-        var line = chart.getSeriesAt(1);
-        line.labels().enabled(true).anchor('right-bottom').format('{%CF}%');
+        const yScaleEnergy = d3.scaleLinear()
+            .domain([0, d3.max(filteredData, d => d.primary_energy_consumption)])
+            .range([height, 0]);
 
-        // turn on the crosshair and set settings
-        chart.crosshair().enabled(true).xLabel(false);
+        // Create x and y axes
+        const xAxis = d3.axisBottom(xScale);
+        const yAxisGDP = d3.axisLeft(yScaleGDP);
+        const yAxisEnergy = d3.axisRight(yScaleEnergy);
 
-        // set the chart container id
-        chart.container('container1');
-        // draw the chart
-        chart.draw();
+        // Remove existing chart elements
+        svg.selectAll(".bar-gdp").remove();
+        svg.selectAll(".line-energy").remove();
+
+        // Draw x axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        // Draw y axis for GDP (left)
+        svg.append("g")
+            .call(yAxisGDP)
+            .attr("class", "axis-left");
+
+        // Draw y axis for primary_energy_consumption (right)
+        svg.append("g")
+            .attr("transform", "translate(" + width + ", 0)")
+            .call(yAxisEnergy)
+            .attr("class", "axis-right");
+
+        // Draw bars for GDP with color grading
+        svg.selectAll(".bar-gdp")
+            .data(filteredData)
+            .enter().append("rect")
+            .attr("class", "bar-gdp")
+            .attr("x", d => xScale(d.year))
+            .attr("y", d => yScaleGDP(d.gdp))
+            .attr("width", xScale.bandwidth())
+            .attr("height", d => height - yScaleGDP(d.gdp))
+            .style("fill", d => colorScale(d.primary_energy_consumption));
+
+        // Draw line for primary_energy_consumption with color grading
+        const line = d3.line()
+            .x(d => xScale(d.year) + xScale.bandwidth() / 2)
+            .y(d => yScaleEnergy(d.primary_energy_consumption));
+
+        svg.append("path")
+            .data([filteredData])
+            .attr("class", "line-energy")
+            .attr("d", line)
+            .style("stroke-width", 2)  // Set the stroke width to 2 pixels
+            .style("stroke", "black");  // Set the stroke color to black
+    }
+
+    // Initial update with the first country in the list
+    updateChart(countries[0]);
+
+    // Add event listener to update the chart when the user selects a different country
+    select.on("change", function () {
+        const selectedCountry = d3.select(this).property("value");
+        updateChart(selectedCountry);
     });
 });
